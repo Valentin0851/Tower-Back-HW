e61d8a828e62a1413b0573a26a11e1ab5bd1382a
Valya <V2lentin2005@yandex.ru>
2024-10-12 22:35:20 +0300

    kkdnjfv

--- hw2/BinaryTreeOnGo.go
+++ hw2/BinaryTreeOnGo.go
@@ -1,31 +1,34 @@
 package BinaryTree
 
-type BST[T any] interface {
+type BST[T ~int | ~float32] interface {
 	isExist(elem T) (error, int) // if exist return index int slice else return error
 	Add(elem T)
 	Delete(elem T)
 }
 
 type Node[T ~int | ~float32] struct {
-	val   T
-	ind   uint
-	left  *Node[T]
-	right *Node[T]
+	val    T
+	ind    uint
+	left   *Node[T]
+	right  *Node[T]
+	parent *Node[T]
 }
 
 func (n *Node[T]) InitEmptyNode() *Node[T] {
 	return &Node[T]{
-		left:  nil,
-		right: nil,
+		left:   nil,
+		right:  nil,
+		parent: nil,
 	}
 }
 
 func (n *Node[T]) InitNodeWithVal(node *Node[T]) *Node[T] {
 	return &Node[T]{
-		val:   node.val,
-		ind:   node.ind,
-		left:  node.left,
-		right: node.right,
+		val:    node.val,
+		ind:    node.ind,
+		left:   node.left,
+		right:  node.right,
+		parent: node.parent,
 	}
 }
 
@@ -40,6 +43,10 @@ func (bst *BinarySearchTree[T]) InitBST() *Node[T] {
 
 func (bst *BinarySearchTree[T]) InitBSTWithVal(rootNode *Node[T]) *Node[T] {
 	return bst.root.InitNodeWithVal(rootNode)
+}
+
+func (bst *BinarySearchTree[T]) findNode(val T, n *Node[T]) {
+
 }
 
 func (bst *BinarySearchTree[T]) FindMin(node *Node[T]) *Node[T] {
@@ -74,5 +81,39 @@ func (bst *BinarySearchTree[T]) Add(val T, node *Node[T]) *Node[T] {
 }
 
 func (bst *BinarySearchTree[T]) Delete(val T, node *Node[T]) *Node[T] {
+	if node == nil {
+		return nil
+	} else if val < node.val {
+		node.left = bst.Delete(val, node.left)
+	} else if val > node.val {
+		node.right = bst.Delete(val, node.right)
+	} else if node.left != nil && node.right != nil {
+		temp := bst.FindMin(node.right)
+		node.val = temp.val
+		node.right = bst.Delete(node.val, node.right)
+	} else {
+		temp := node
+		if node.left == nil {
+			node = node.right
+		} else if node.right == nil {
+
+		}
+	}
 	return node
+}
+
+func (bst *BinarySearchTree[T]) isExist(val T, n *Node[T]) *Node[T] {
+	if n == nil {
+		return nil
+	}
+	if n.val == val {
+		return n
+	}
+
+	if n.val < val {
+		return bst.isExist(val, n.right)
+	} else {
+		return bst.isExist(val, n.left)
+	}
+
 }

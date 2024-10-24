9df2bf2c1c73496c61e8e863193b48616d0e73c0
Valya <V2lentin2005@yandex.ru>
2024-10-24 19:06:12 +0300

    done

--- hw3/uniq.go
+++ hw3/uniq.go
@@ -7,63 +7,163 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // type Options struct {
-// 	inputChan  io.Reader
-// 	outputChan io.Writer
+//   inputChan  io.Reader
+//   outputChan io.Writer
 // }
 
 // func newOptinos1(s io.Reader) *Options {
-// 	return &Options{
-// 		inputChan: s,
-// 	}
+//   return &Options{
+//     inputChan: s,
+//   }
 // }
 
 // func newOptinos2(s1 io.Reader, s2 io.Writer) *Options {
-// 	return &Options{
-// 		inputChan:  s1,
-// 		outputChan: s2,
-// 	}
+//   return &Options{
+//     inputChan:  s1,
+//     outputChan: s2,
+//   }
 // }
 
-func UniqStrings(input io.Reader, output io.Writer) error {
-	in := bufio.NewScanner(input)
-	prevStr := ""
-	for in.Scan() {
-		txt := in.Text()
-		if txt != prevStr {
-			output.Write([]byte(txt + "\n"))
-		}
-		prevStr = txt
-	}
-	return nil
-}
-
-func cFlag(input io.Reader, output io.Writer) {
-	in := bufio.NewScanner(input)
-	prevStr := ""
-	cnt := 1
-	for in.Scan() {
-		txt := in.Text()
-		if txt != prevStr {
-			output.Write([]byte(strconv.Itoa(cnt) + " " + txt + "\n"))
+func UniqStrings(dataSlice, other []string, output io.Writer) {
+	prevStr := ":wq!"
+	cnt := 0
+	for i, val := range other {
+		if val != prevStr && prevStr != ":wq!" {
+			output.Write([]byte(dataSlice[i-cnt] + "\n"))
 			cnt = 0
 		}
+		prevStr = val
 		cnt++
-		prevStr = txt
+	}
+	if len(dataSlice) > 2 && other[len(dataSlice)-1] != other[len(dataSlice)-2] {
+		output.Write([]byte(dataSlice[len(dataSlice)-1]))
+	} else {
+		output.Write([]byte(dataSlice[len(dataSlice)-cnt]))
 	}
 }
 
-func main() {
-	// var cFlag *int
-	// var dFlag, uFlag, f_num_fieldsFlag, s_num_charsFlag, iFlag string
-	// flag.IntVar(cFlag, "c", 0, "count of every string")
-	// flag.Func("c", "Count of every string in input", func(s string) error {
+func UniqStringsWithc(dataSlice, other []string, output io.Writer) {
+	cnt := 0
+	prevStr := ":wq!"
+	for i, val := range other {
+		if val != prevStr && prevStr != ":wq!" {
+			output.Write([]byte(strconv.Itoa(cnt) + " " + dataSlice[i-cnt] + "\n"))
+			cnt = 0
+		}
+		prevStr = val
+		cnt++
+	}
+	if other[len(other)-1] != other[len(other)-2] {
+		output.Write([]byte("1" + " " + dataSlice[len(dataSlice)-1] + "\n"))
+	} else {
+		output.Write([]byte(strconv.Itoa(cnt) + " " + dataSlice[len(dataSlice)-cnt] + "\n"))
+	}
+}
 
-	// })
+func UniqStringsWithd(dataSlice, other []string, output io.Writer) {
+	cnt := 0
+	prevStr := ":wq!"
+	for i, val := range other {
+		if val != prevStr && prevStr != ":wq!" {
+			if cnt > 1 {
+				output.Write([]byte(dataSlice[i-cnt] + "\n"))
+			}
+			cnt = 0
+		}
+		prevStr = val
+		cnt++
+	}
+	if cnt > 1 && other[len(dataSlice)-cnt] == prevStr {
+		output.Write([]byte(prevStr + "\n"))
+	}
+}
+
+func UniqStringsWithu(dataSlice, other []string, output io.Writer) {
+	cnt := 0
+	prevStr := ":wq!"
+	for i, val := range other {
+		if val != prevStr && prevStr != ":wq!" {
+			if cnt == 1 {
+				output.Write([]byte(dataSlice[i-cnt] + "\n"))
+			}
+			cnt = 0
+		}
+		prevStr = val
+		cnt++
+	}
+	if other[len(dataSlice)-1] != other[len(dataSlice)-2] {
+		output.Write([]byte(dataSlice[len(dataSlice)-1] + "\n"))
+	}
+}
+
+func UniqStringsWithi(dataSlice []string) []string {
+	var ans []string
+	for _, val := range dataSlice {
+		ans = append(ans, strings.ToLower(val))
+	}
+	return ans
+}
+
+func UniqStringsWithNumFields(other []string, numFields int) []string {
+	var ans []string
+	for _, val := range other {
+		lineSplit := strings.Fields(val)
+		if len(lineSplit) > numFields {
+			ans = append(ans, strings.Join(lineSplit[numFields:], " "))
+		} else {
+			ans = append(ans, "")
+		}
+	}
+	return ans
+}
+
+func UniqStringsWithNumChars(other []string, numChars int) []string {
+	var ans []string
+	for _, val := range other {
+		if len(val) > numChars {
+			line := ""
+			for i := range val {
+				if i >= numChars {
+					line += string(val[i])
+				}
+			}
+			ans = append(ans, line)
+		} else {
+			ans = append(ans, "")
+		}
+	}
+	return ans
+}
+
+func readDataFromChan(input io.Reader) []string {
+	var ans []string
+	in := bufio.NewScanner(input)
+	for in.Scan() {
+		ans = append(ans, in.Text())
+	}
+	return ans
+}
+
+// func writeDataToChan(dataSlice []string, output io.Writer){
+//   for _,val := range dataSlice{
+//     output.Write([]byte(val+"\n"))
+//   }
+// }
+
+func main() {
+	cFlag := flag.Bool("c", false, "count of every string") // print string with number of repetition
+	dFlag := flag.Bool("d", false, "only repeated strings") // print repeated strings
+	uFlag := flag.Bool("u", false, "uniq strings")          // print uniq strings
+	iFlag := flag.Bool("i", false, "Ignore case")
+	numFieldsFlag := flag.Int("f", 0, "number of fiels which i need to miss")
+	numCharsFlag := flag.Int("s", 0, "number of chars without spaces which i need miss")
 
 	flag.Parse()
+	var other []string
 
 	cntPosArgs := len(flag.Args())
 	if cntPosArgs == 2 {
@@ -77,7 +177,33 @@ func main() {
 		}
 		defer inFile.Close()
 		defer outFile.Close()
-		UniqStrings(inFile, outFile)
+		dataSlice := readDataFromChan(inFile)
+		if *iFlag {
+			other = UniqStringsWithi(other)
+		}
+		if *numFieldsFlag != 0 {
+			other = UniqStringsWithNumFields(other, *numFieldsFlag)
+		}
+		if *numCharsFlag != 0 {
+			other = UniqStringsWithNumChars(other, *numCharsFlag)
+		}
+		if *cFlag || *dFlag || *uFlag {
+			if *cFlag {
+				UniqStringsWithc(dataSlice, other, outFile)
+				return
+			}
+			if *dFlag {
+				UniqStringsWithd(dataSlice, other, outFile)
+				return
+			}
+			if *uFlag {
+				UniqStringsWithu(dataSlice, other, outFile)
+				return
+			}
+		} else {
+			UniqStrings(dataSlice, other, outFile)
+			return
+		}
 	}
 
 	if cntPosArgs == 1 {
@@ -86,9 +212,62 @@ func main() {
 			log.Fatal("Error opening input file")
 		}
 		defer inFile.Close()
-		UniqStrings(inFile, os.Stdout)
+		dataSlice := readDataFromChan(inFile)
+		other = dataSlice
+		if *iFlag {
+			other = UniqStringsWithi(other)
+		}
+		if *numFieldsFlag != 0 {
+			other = UniqStringsWithNumFields(other, *numFieldsFlag)
+		}
+		if *numCharsFlag != 0 {
+			other = UniqStringsWithNumChars(other, *numCharsFlag)
+		}
+		if *cFlag || *dFlag || *uFlag {
+			if *cFlag {
+				UniqStringsWithc(dataSlice, other, os.Stdout)
+				return
+			}
+			if *dFlag {
+				UniqStringsWithd(dataSlice, other, os.Stdout)
+				return
+			}
+			if *uFlag {
+				UniqStringsWithu(dataSlice, other, os.Stdout)
+				return
+			}
+		} else {
+			UniqStrings(dataSlice, other, os.Stdout)
+			return
+		}
 	}
 
-	UniqStrings(os.Stdin, os.Stdout)
-
+	dataSlice := readDataFromChan(os.Stdin)
+	other = dataSlice
+	if *iFlag {
+		other = UniqStringsWithi(other)
+	}
+	if *numFieldsFlag != 0 {
+		other = UniqStringsWithNumFields(other, *numFieldsFlag)
+	}
+	if *numCharsFlag != 0 {
+		other = UniqStringsWithNumChars(other, *numCharsFlag)
+	}
+	if *cFlag || *dFlag || *uFlag {
+		if *cFlag {
+			UniqStringsWithc(dataSlice, other, os.Stdout)
+			return
+		}
+		if *dFlag {
+			UniqStringsWithd(dataSlice, other, os.Stdout)
+			return
+		}
+		if *uFlag {
+			UniqStringsWithu(dataSlice, other, os.Stdout)
+			return
+		}
+	} else {
+		UniqStrings(dataSlice, other, os.Stdout)
+		return
+	}
 }
